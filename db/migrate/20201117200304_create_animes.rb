e6914dd71cb9a47968217d21af3ff65c66de281c
Lien Nguyen <lien@Liens-MacBook-Air.local>
2020-11-17 12:03:32 -0800

    anime model created

---
+++ db/migrate/20201117200304_create_animes.rb
@@ -0,0 +1,11 @@
+class CreateAnimes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :animes do |t|
+      t.string :name
+      t.string :image_url
+      t.string :slug
+
+      t.timestamps
+    end
+  end
+end
