4cb0107ef5e94cc391531505b02d8bd45754d51a
Lien Nguyen <lien@Liens-MacBook-Air.local>
2020-11-30 08:00:40 -0800

    add linkedin icon to footer

--- app/javascript/components/Footer/Footer.js
+++ app/javascript/components/Footer/Footer.js
@@ -2,7 +2,12 @@ import React from 'react'
 import './Footer.css'
 
 const Footer = () => {
-    return <p className='footer'>{'\u00A9'} 2020 AnimeWatch All Rights Reserved</p>;
+    return (
+        <div className='footer'>
+            <p>{'\u00A9'} Lien Nguyen - 2020 AnimeWatch All Rights Reserved</p>
+            <p>Connect @ <a href="https://www.linkedin.com/in/lien-nguyen-575aba153/"><i className="fab fa-linkedin linkedin"></i></a></p>
+        </div>
+    ) 
 }
 
 export default Footer
