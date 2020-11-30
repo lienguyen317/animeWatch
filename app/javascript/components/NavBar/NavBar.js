d5c07396b6c018989e1e2d9ee4a3937fb12045a4
Lien Nguyen <lien@Liens-MacBook-Air.local>
2020-11-30 00:25:53 -0800

    fixed navbar

--- app/javascript/components/NavBar/NavBar.js
+++ app/javascript/components/NavBar/NavBar.js
@@ -8,8 +8,8 @@ const NavBar = () => {
             <nav>
                 <div className='logo'><b>Anime</b>&nbsp;<i className="far fa-clock watch"></i></div>
                 <ul className='nav-links'>
-                    <li><a href='/'><b>Home</b>&nbsp;<i class="fas fa-home"></i></a></li>
-                    <li><a href='/about'><b>About</b>&nbsp;<i class="fas fa-info-circle"></i></a></li>
+                    <li><a href='/'><b>Home</b>&nbsp;<i className="fas fa-home"></i></a></li>
+                    <li><a href='/about'><b>About</b>&nbsp;<i className="fas fa-info-circle"></i></a></li>
                 </ul>
             </nav>
         </div>
